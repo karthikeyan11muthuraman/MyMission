1099ed971a61c9a7eaceda6065e2b6ec7ac5db93
karthikeyan11muthuraman <karthikeyan11muthuraman@gmail.com>
2024-06-01 13:46:03 +0530

    Add files via upload
    Leaftap launch page code

---
+++ leaftap/LaunchWeb.java
@@ -0,0 +1,9 @@
+package leaftap;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchWeb {
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+	}
+}
